8341bb724efffad669caff821e87f1d461b199d0
mrtaz77 <mrtazwar23@gmail.com>
2023-06-14 19:26:16 +0600

    Don't care + ratio

--- CPP/Graph/DisjointSet.h
+++ CPP/Graph/DisjointSet.h
@@ -5,7 +5,7 @@ class DisjointSet{
 public:
     DisjointSet(int n){
         rank.resize(n+1,0);
-        parent.resize(n);
+        parent.resize(n+1);
         for(int i=0;i<=n;i++)parent[i]=i;
     }
 
