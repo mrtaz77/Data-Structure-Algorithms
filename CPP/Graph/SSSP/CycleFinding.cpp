bf430ff60b07fcfadebb5d300bece26d534a8dd1
mrtaz77 <mrtazwar23@gmail.com>
2023-06-22 12:29:06 +0600

    Cycle Finding Optimized

--- CPP/Graph/SSSP/CycleFinding.cpp
+++ CPP/Graph/SSSP/CycleFinding.cpp
@@ -1,38 +1,40 @@
+//CSES Cycle Finding
+//Negative cycle detection over the entire graph
+//Trick : do bellford with start distance 0
+//equivalent of using an imaginary node at 0 distance from every node as starting point node 
 #include<bits/stdc++.h>
 using namespace std;
 void addEdge(vector<pair<int,long double>> adj[],int u,int v,long double w=1.0){adj[u].push_back({v,w});}
 
 void solve(vector<pair<int,long double>> adj[],int n){
     vector<pair<int,long double>> bf(n,{-1,0});
-    for(int i=0;i<n-1;i++){
+    int k = -1;
+    for(int i=0;i<n;i++){
         for(int j=0;j<n;j++){
             for(auto x:adj[j]){
                 if(bf[j].second + x.second < bf[x.first].second){
                     bf[x.first].second = bf[j].second + x.second;
                     bf[x.first].first = j;
+                    if(i == n-1)k = x.first;
                 }
             }
         }
     }
-    for(int j=0;j<n;j++){
-        for(auto x:adj[j]){
-            if(bf[j].second + x.second < bf[x.first].second){
-                cout<<"YES\n";
-                vector<int> cycle;
-                int k = x.first;
-                for(int i=0;i<n;i++)k = bf[k].first;
-                int v = k;
-                cycle.push_back(v);
-                do{
-                    k = bf[k].first;
-                    cycle.push_back(k);
-                }while(k != v && cycle.size() > 1);
-                for(int i=0;i<cycle.size();i++)cout<<cycle[cycle.size()-i-1]+1<<" ";
-                return;
-            }
-        }
+    if(k == -1){
+        cout<<"NO\n";
     }
-    cout<<"NO\n";
+    else{
+        cout<<"YES\n";
+        vector<int> cycle;
+        for(int i=0;i<n;i++)k = bf[k].first;
+        int v = k;
+        cycle.push_back(v);
+        do{
+            k = bf[k].first;
+            cycle.push_back(k);
+        }while(k != v);
+        for(int i=0;i<cycle.size();i++)cout<<cycle[cycle.size()-i-1]+1<<" ";
+    }
 }
 
 
