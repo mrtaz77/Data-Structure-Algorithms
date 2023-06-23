1213ca93dc29f22d94af217b6903db4416714f43
mrtaz77 <mrtazwar23@gmail.com>
2023-06-24 00:39:06 +0600

    GraphUtil updated max distance and data types

--- CPP/Graph/SSSP/GraphUtil.h
+++ CPP/Graph/SSSP/GraphUtil.h
@@ -1,7 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
-double edgeWeight(vector<pair<int,double>> adj[],int u,int v){
-    double weight = INFINITY;
+long long edgeWeight(vector<pair<int,long long>> adj[],int u,int v){
+    long long weight = 1e17;
     for(auto x:adj[u]){
         if(x.first==v && x.second < weight){
             weight = x.second;
@@ -10,15 +10,15 @@ double edgeWeight(vector<pair<int,double>> adj[],int u,int v){
     return weight;	
 }
 
-void rmEdge(vector<pair<int,double>> adj[],int u,int v){
+void rmEdge(vector<pair<int,long long>> adj[],int u,int v){
     for(int i=0;i<adj[u].size();i++){
         if(adj[u][i].first == v)adj[u].erase(adj[u].begin()+i);
     }
 }
 
-void addEdge(vector<pair<int,double>> adj[],int u,int v,double w=1.0){adj[u].push_back({v,w});}
+void addEdge(vector<pair<int,long long>> adj[],int u,int v,long long w=1.0){adj[u].push_back({v,w});}
 
-void print(vector<pair<int,double>> adj[],int n){
+void print(vector<pair<int,long long>> adj[],int n){
     for(int i=0;i<n;i++){
         cout<<i<<" -> ";
         for(auto x:adj[i]){
