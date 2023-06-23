bcd7ce714a765e6bc5ee6a4c6b5c89c6ede0761e
mrtaz77 <mrtazwar23@gmail.com>
2023-06-24 00:39:31 +0600

    High Score submission success!

--- CPP/Graph/SSSP/HighScore.cpp
+++ CPP/Graph/SSSP/HighScore.cpp
@@ -1,14 +1,15 @@
 #include<bits/stdc++.h>
 using namespace std;
-void addEdge(vector<pair<int,long double>> adj[],int u,int v,long double w=1.0){adj[u].push_back({v,w});}
+void addEdge(vector<pair<int,long long int>> adj[],int u,int v,long long int w=1.0){adj[u].push_back({v,w});}
 
-int solve(vector<pair<int,long double>> adj[],int n){
-    vector<pair<int,long double>> bf(n,{-1,0});
+long long int solve(vector<pair<int,long long int>> adj[],int n){
+    vector<pair<int,long long int>> bf(n,{-1,1e17});
+    bf[0].second = 0;
     int k = -1;
     for(int i=0;i<n;i++){
         for(int j=0;j<n;j++){
             for(auto x:adj[j]){
-                if(bf[j].second + x.second < bf[x.first].second){
+                if(bf[j].second != 1e17 && bf[j].second + x.second < bf[x.first].second){
                     bf[x.first].second = bf[j].second + x.second;
                     bf[x.first].first = j;
                     if(i == n-1)k = x.first;
@@ -16,31 +17,35 @@ int solve(vector<pair<int,long double>> adj[],int n){
             }
         }
     }
-    if(k == -1){
-        int max = bf[0].second;
-        for(int i = 1; i < n;i++){
-            if(bf[i].second < max)max = bf[i].second;
+    if(k != -1){
+        bf[k].second = -1e17;
+        for(int i=0;i<n-1;i++){
+            for(int j=0;j<n;j++){
+                for(auto x:adj[j]){
+                    if(bf[j].second != 1e17 && bf[j].second + x.second < bf[x.first].second){
+                        bf[x.first].second = -1e17;
+                    }
+                }
+            }
         }
-        return -max;
+        if(bf[n-1].second == -1e17)return -1;
     }
-    else{
-        return -1;
-    }
+    return -bf[n-1].second; 
 }
 
 int main(){
-    // freopen("input.txt", "r", stdin);
-    // freopen("output.txt", "w", stdout);
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
 
     int n,m;
     cin >> n >> m;
 
     
-    vector<pair<int,long double>> *adjBF = new vector<pair<int,long double>>[n];
+    vector<pair<int,long long int>> *adjBF = new vector<pair<int,long long int>>[n];
 
     for (int i = 0; i < m; ++i){
         int u,v;
-        long double w;
+        long long int w;
         cin >> u >> v >> w;
         //Maximize distance , so -w
         addEdge(adjBF,u-1,v-1,-w);
