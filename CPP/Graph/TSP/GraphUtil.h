1be6d4a6327a1d451767780484edeb2015221f52
mrtaz77 <mrtazwar23@gmail.com>
2023-09-11 03:16:27 +0600

    offline done

--- CPP/Graph/TSP/GraphUtil.h
+++ CPP/Graph/TSP/GraphUtil.h
@@ -29,38 +29,59 @@ Graph Create_Random_Graph(int V){
     return g;
 }
 
-long double bitmaskDp(int current,long int mask,Graph &G,vector<vector<int>>& dp){
+long double bitmaskDp(int current,long int mask,Graph &G,vector<vector<pair<int,int>>>& dp){
     
     if(mask == (1 << G.getV()) - 1){
         // cout<<current<<" -> "<<0<<endl;
         // cout<<"=="<<endl;
+        dp[mask][current].second = 0; 
         return G.edgeWeight(current,0);
     }
 
-    if(dp[mask][current] != -1){
+    if(dp[mask][current].first != -1){
         // cout<<"++"<<endl;
-        return dp[mask][current];
+        return dp[mask][current].first;
     }
 
     long double ans = MAXG;
-    int j = -1;
 
     for(int i=0;i < G.getV();i++){
         if(!(mask & (1 << i))){
             long double result = G.edgeWeight(current,i)+bitmaskDp(i,mask|(1<<i),G,dp);
             // if(result < ans)cout<<"--"<<endl;
-            ans = min(ans,result);
+            if(ans > result){
+                ans = result;
+                dp[mask][current].second = i;
+            }
         }
     }
     // cout<<current<<" "<<endl;
-    dp[mask][current] = ans;
+    dp[mask][current].first = ans;
     return ans;
 }
 
-long double Exact_TSP(Graph& G){
-    vector<vector<int>> dp((1 << G.getV()), vector<int>(G.getV(), -1));
+vector<int> Exact_TSP(Graph& G){
+    vector<vector<pair<int,int>>> dp((1 << G.getV()), vector<pair<int,int>>(G.getV(), {-1,-1}));
     // cout<<"Exact_TSP : "<<endl;
-    auto tour = bitmaskDp(0,1,G,dp);
+    auto tourLength = bitmaskDp(0,1,G,dp);
+
+    vector<int> tour;
+    tour.push_back(0);
+
+    int v = 0;
+    int i = 1;
+    while(tour.size() <= G.getV()){
+        if(dp[i][v].second != -1){
+            // cout<<tour.size()<<endl;
+            tour.push_back(dp[i][v].second);
+            v = dp[i][v].second ;
+            i = i | (1 << v);
+        }
+    }
+    
+    // for(int i = 0; i <tour.size(); i++)cout<<tour[i]<<" -> ";
+    // cout<<endl;
+
     return tour;
 }
 
@@ -143,7 +164,7 @@ long double Calculate_Tour_Length(Graph& G,vector<int> &vertices){
 
 }
 
-long double Metric_Approximation_TSP(Graph& G){
+vector<int> Metric_Approximation_TSP(Graph& G){
     Graph mst = primMST(G,0);
 
     // cout<<mst<<endl;
@@ -152,7 +173,7 @@ long double Metric_Approximation_TSP(Graph& G){
 
     // for(int i=0;i<dfsTree.size();i++)cout<<dfsTree[i]<<" ";
     // cout<<endl;
-    return Calculate_Tour_Length(G,dfsTree);
+    return dfsTree;
     // return 0;
 }
 
--- CPP/Graph/TSP/main.cpp
+++ CPP/Graph/TSP/main.cpp
@@ -1,10 +1,10 @@
-#define V 20
+#define vertexCount 20
 #include<bits/stdc++.h>
 #include"GraphUtil.h"
 using namespace std;
 
 int main(){
-    srand(time(NULL));
+    srand(20);
     freopen("in.txt", "r", stdin);
     freopen("out.txt", "w", stdout);
 
@@ -16,12 +16,29 @@ int main(){
     long double *approxTour = new long double[t];
 
     for(int i = 0; i < t; i++){
-        Graph g = Create_Random_Graph(V);
+        Graph g = Create_Random_Graph(vertexCount);
         // cout<<g;
 
-        exactTour[i] = Exact_TSP(g);
-        approxTour[i] = Metric_Approximation_TSP(g);
-        cout <<"("<<i+1<<", " <<approxTour[i]/exactTour[i]<<")" << endl;
+        auto bitTour = Exact_TSP(g);
+        exactTour[i] = Calculate_Tour_Length(g,bitTour); 
+        auto appTour = Metric_Approximation_TSP(g);
+        approxTour[i] = Calculate_Tour_Length(g,appTour);
+
+        // for(int i = 0; i < bitTour.size(); i++){
+        //     cout<<bitTour[i];
+        //     if(i != bitTour.size() -1)cout<<" -> ";
+        // }
         // cout<<endl;
+
+        // for(int i = 0; i < appTour.size(); i++){
+        //     cout<<appTour[i];
+        //     if(i != appTour.size() -1)cout<<" -> ";
+        // }
+        // cout<<endl;
+
+        cout <<"("<<i+1<<", " <<approxTour[i]/exactTour[i]<<")" << endl;
+        cout<<endl;
     }
+    delete approxTour;
+    delete exactTour;
 }
